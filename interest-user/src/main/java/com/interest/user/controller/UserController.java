cb5660248c3f428761ba3418e475917ce1a141e6
smallsnail-wh <1147462829@qq.com>
2019-04-02 00:02:59 +0800

    zuul server create

---
+++ interest-user/src/main/java/com/interest/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.interest.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "SUCCESS";
+    }
+
+}
